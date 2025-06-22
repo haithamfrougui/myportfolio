a423fb51d2caff4846dee9b979f9d66594ae81b7
haitham <haitham.frougui@usmba.ac.ma>
2025-06-22 19:58:16 +0100

    style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -38,7 +38,7 @@ dayNight.addEventListener('click', () => {
 });
 
 window.addEventListener('load', () => {
-  if (document.body.classList.contains('light')) {
+  if (document.body.classList.contains('dark')) {
     dayNight.querySelector('i').classList.add('fa-moon');
   } else {
     dayNight.querySelector('i').classList.add('fa-sun');
